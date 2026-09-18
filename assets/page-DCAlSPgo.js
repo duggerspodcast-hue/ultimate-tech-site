317370e77acc7c16af3f77ef429b35d1d25dfe7f
Andrey <korobov.and@gmail.com>
2026-09-18 15:55:48 +0300

    Publish refined Akron fantasy screen

---
+++ assets/page-DCAlSPgo.js
@@ -0,0 +1,1 @@
+import{r as e}from"./framework-CXnKph_e.js";import{l as t}from"./index-D8ltnDAT.js";import n from"./AdminShell-Dl5azKqP.js";var r=e();function i(){return(0,r.jsx)(n,{view:`supplier-detail`,supplierId:t().get(`id`)||``})}export{i as default};
